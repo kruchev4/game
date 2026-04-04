3256233cc67452421fedbf3e59650c9095241b68
kruchev4 <120105804+kruchev4@users.noreply.github.com>
2026-04-04 11:50:59 -0400

    Create tiles.js

---
+++ src/data/tiles.js
@@ -0,0 +1,7 @@
+export const TILES = {
+  0: { name: "void",      walkable: false },
+  1: { name: "grass",     walkable: true },
+  2: { name: "forest",   walkable: true },
+  3: { name: "mountain", walkable: false },
+  4: { name: "water",    walkable: false }
+};
