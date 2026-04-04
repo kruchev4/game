76ad1b972da878f31713c68f5da07a5ace421357
kruchev4 <120105804+kruchev4@users.noreply.github.com>
2026-04-04 12:06:39 -0400

    Update tiles.js

--- src/data/tiles.js
+++ src/data/tiles.js
@@ -1,7 +1,27 @@
 export const TILES = {
-  0: { name: "void",      walkable: false },
-  1: { name: "grass",     walkable: true },
-  2: { name: "forest",   walkable: true },
-  3: { name: "mountain", walkable: false },
-  4: { name: "water",    walkable: false }
+  0: {
+    name: "void",
+    walkable: false,
+    color: "#000000"
+  },
+  1: {
+    name: "grass",
+    walkable: true,
+    color: "#3fa34d"
+  },
+  2: {
+    name: "forest",
+    walkable: true,
+    color: "#267a3e"
+  },
+  3: {
+    name: "mountain",
+    walkable: false,
+    color: "#7a7a7a"
+  },
+  4: {
+    name: "water",
+    walkable: false,
+    color: "#2b65c2"
+  }
 };
