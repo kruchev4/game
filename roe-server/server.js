2dd6d26b87d9c99a200389cd2db8a33f10037bf7
kruchev4 <120105804+kruchev4@users.noreply.github.com>
2026-04-25 15:43:22 -0400

    Update server.js

--- roe-server/server.js
+++ roe-server/server.js
@@ -11,7 +11,7 @@
 
 require("dotenv").config();
 const { WebSocketServer, WebSocket } = require("ws");
-
+const path = require("path");
 
 const fs       = require("fs");
 const http     = require("http");
